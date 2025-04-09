a2023384c1eae2343fca02486e73ac6b2e375196
chandan-nayak <chandan_nayak@apple.com>
2025-04-09 19:16:27 +0530

    updated sg

--- terraform_codes/sg.tf
+++ terraform_codes/sg.tf
@@ -30,30 +30,3 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-/*resource "aws_security_group" "app_sg" {
-  name        = "node-app-sg"
-  description = "Ingress for vault"
-  vpc_id      = aws_vpc.rearc.id
-
-  dynamic "ingress" {
-    for_each = var.sg_ports
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-  }
-  dynamic "egress" {
-    for_each = var.sg_ports
-    content {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-  }
-}*/
