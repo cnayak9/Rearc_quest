ddba239c827dbe471dbe35766998a2ccd7e99fd8
Chandan Nayak <51763297+cnayak9@users.noreply.github.com>
2025-04-10 23:23:56 +0530

    Update sg.tf
    Updating SG

--- terraform_codes/sg.tf
+++ terraform_codes/sg.tf
@@ -1,3 +1,4 @@
+#####Security group for ec2###############
 resource "aws_security_group" "app_sg" {
   name   = "node-app-sg"
   vpc_id = aws_vpc.rearc.id
@@ -15,7 +16,7 @@ resource "aws_security_group" "app_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+########## port 22 cidr use as per requirement#########
   ingress {
     from_port   = 22
     to_port     = 22
@@ -31,32 +32,7 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-/*resource "aws_security_group" "app_sg" {
-  name        = "node-app-sg"
-  description = "Ingress for vault"
-  vpc_id      = aws_vpc.rearc.id
-
-  dynamic "ingress" {
-    for_each = var.sg_ports
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-  }
-  dynamic "egress" {
-    for_each = var.sg_ports
-    content {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-
-  }
-}*/
+#######Security Group for ALB###############
 resource "aws_security_group" "alb_sg" {
   name        = "rearc-alb_sg"
   description = "Allow HTTP to ALB"
